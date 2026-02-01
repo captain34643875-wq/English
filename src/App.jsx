6f55e441286328718b28e35eb3b59e0347f614bd
ayj8201 <junny.on.top@gmail.com>
2026-02-01 17:00:12 +0900

    Fix typo in home header paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           <Route path="/" element={
             <header className="home-header">
               <h1>⚡ 가성비 영어 학습 서비스</h1>
-              <p>원하는 학습 모드를 선택해봐, 유준아!</p>
+              <p>원하는 학습 모드를 선택해봐!</p>
               {/* 홈 화면에서도 바로 갈 수 있게 버튼 하나 만들어둘까? */}
               <Link to="/voca"><button>단어 공부 시작하기</button></Link>
             </header>
